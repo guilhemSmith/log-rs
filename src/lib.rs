803b89918ddf8c5dc987f069f6c1a79e475defb1
guilhem smith <smith.guilhem@gmail.com>
2020-04-03 19:39:45 +0200

    Functions to set the outputs in Logger struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 mod timestamp;
 mod logger;
 
-pub use logger::Logger;
+pub use logger::{Logger, OutputKind};
 
 #[cfg(test)]
 mod tests {
@@ -11,24 +11,28 @@ mod tests {
     #[test]
     fn info() -> io::Result<()> {
         let mut log = Logger::new();
+        log.set_output_info(OutputKind::FILE("infolog.txt"));
         log.info("this is an info.")
     }
 
     #[test]
     fn debug() -> io::Result<()> {
         let mut log = Logger::new();
+        log.set_output_debug(OutputKind::FILE("debuglog.txt"));
         log.debug("this is a debug.")
     }
 
     #[test]
     fn error() -> io::Result<()> {
         let mut log = Logger::new();
+        log.set_output_error(OutputKind::FILE("errorlog.txt"));
         log.error("this is an error.")
     }
 
     #[test]
     fn warning() -> io::Result<()> {
         let mut log = Logger::new();
+        log.set_output_warning(OutputKind::FILE("warninglog.txt"));
         log.warning("this is a warning.")
     }
 }
