ad9b820d6066542ca2242e6fbf9250758b36ae8c
guilhem smith <smith.guilhem@gmail.com>
2020-04-08 15:44:46 +0200

    Timestamp use local time.

--- src/timestamp.rs
+++ src/timestamp.rs
@@ -1,5 +1,5 @@
 extern crate chrono;
-use chrono::Utc;
+use chrono::Local;
 use std::string::ToString;
 
 /// Get the current timestamp formated to apply to the ISO 8601.
@@ -7,6 +7,6 @@ use std::string::ToString;
 ///
 /// A String containing the current timestamp.
 pub fn get_timestamp() -> String {
-    let now = Utc::now();
+    let now = Local::now();
     return now.format("%Y-%m-%dT%H:%M:%S%z").to_string();
 }
